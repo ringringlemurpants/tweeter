51e5f73f32eaa0afb3ffb6d06cc9149da49a2e0c
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-23 19:55:30 +0000

    changes counter color to .red when < 1

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,22 @@
 $(function() {
-  //reduces ".counter" by number of characters in ".tweet-input" textarea
+  //reduce ".counter" by number of characters in ".tweet-input" textarea
   $( ".tweet-input" ).keyup(function() {
+    //updates number in counter
     const tweetContent = $(this).val();
+    //const currentChars = tweetContent.length;
+    // const reducedCount = 140 - currentChars;
     const reducedCount = 140 - tweetContent.length;
     $(this).siblings( ".counter" ).html(reducedCount);
+    //updates colour based on currentCount
+    //console.log(tweetContent.length);
+    if (tweetContent.length > 139) {
+      $( ".counter" ).toggleClass( "red", true );
+    } else {
+      $( ".counter" ).toggleClass( "red", false );
+    }
   });
-
 });
+
+// else if (tweetContent.length > 129 && tweetContent.length < 139) {
+//   $( ".counter" ).toggleClass( "orange", true );
+// }
