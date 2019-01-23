4c8f12dc03afef6916c3d9aac723b04b4c26d347
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-23 20:12:43 +0000

    changes colour of .counter to orange when < 10 char to limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,15 +8,15 @@ $(function() {
     const reducedCount = 140 - tweetContent.length;
     $(this).siblings( ".counter" ).html(reducedCount);
     //updates colour based on currentCount
-    //console.log(tweetContent.length);
+    console.log(tweetContent.length);
     if (tweetContent.length > 139) {
-      $( ".counter" ).toggleClass( "red", true );
+      $( ".counter" ).toggleClass( "red", true);
+    } else if (tweetContent.length > 129 && tweetContent.length < 140) {
+      $( ".counter" ).toggleClass( "orange", true);
+      $( ".counter" ).toggleClass( "red", false);
     } else {
-      $( ".counter" ).toggleClass( "red", false );
+      $( ".counter" ).toggleClass( "orange", false);
+      $( ".counter" ).toggleClass( "red", false);
     }
   });
 });
-
-// else if (tweetContent.length > 129 && tweetContent.length < 139) {
-//   $( ".counter" ).toggleClass( "orange", true );
-// }
