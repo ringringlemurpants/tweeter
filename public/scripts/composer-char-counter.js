36a71a0b17ce6e21f6ae261e1fdc7c42138a2990
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-23 03:35:58 +0000

    adds jq function to update <span> counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,15 @@
-console.log("WOO");
+$(function() {
+  //get.querySelector("#tweet-area");
+
+  $(".tweet-area").keyup(function() {
+    const content = $( ".tweet-area" ).val();
+    const reducedCount = 140 - content.length;
+    $(".counter").html(reducedCount);
+    //console.log(this);
+    //if ($(".counter").html(reducedCount) <= 0)
+
+  });
+
+
+
+});
