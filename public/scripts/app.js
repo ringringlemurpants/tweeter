d06840f8e8e61cd57d6b750d5d03c42284867169
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-23 00:38:13 +0000

    alignes compose tweet box centered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// v--like this--v
+// $(function() {
+
+// });
+// ^--like this--^
+
+
+//EXAMPLE F0R W2D3 NOTES:
+
+// $(function() {
+
+//   function btnClick() {
+//     $("#tweet").on('click', function(event){
+//       event.preventDefault();
+//       alert("You can't send an empty tweet, dude.");
+//     });
+//   }
+// });
