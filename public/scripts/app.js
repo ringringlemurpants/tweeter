b9d7ed7a78383701b064d889e3a72a0256dbb15d
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-24 02:02:44 +0000

    changes styling of compose-tweet input and counter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,21 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// v--like this--v
-// $(function() {
+$(function() {
 
-// });
-// ^--like this--^
+  createTweetElement
 
-
-//EXAMPLE F0R W2D3 NOTES:
-
-// $(function() {
-
-//   function btnClick() {
-//     $("#tweet").on('click', function(event){
-//       event.preventDefault();
-//       alert("You can't send an empty tweet, dude.");
-//     });
-//   }
-// });
+});
