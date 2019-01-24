9846e8ed9145173b17a2cdcb37f271e1dbdd4144
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-24 22:53:26 +0000

    renderTweets func now pushes information from tweetData to generate multiple tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,31 +6,73 @@
  */
 
 $(function() {
+  // Fake data taken from tweets.json
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png" },
-      "handle": "@SirIsaac" },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants" },
-    "created_at": 1461116232227 };
+  function renderTweets(tweetsArr) {
+    for (let key in tweetsArr) {
+      let tweet = createTweetElement(tweetsArr[key]);
+      $( "#published-tweets" ).append(tweet);
+    }
+  }
 
-  function createTweetElement(object) {
+  function createTweetElement(tweet) {
     //defines variables
     const $eachTweet = $( "<article>" ).addClass( "each-tweet" );
     const $eachTweetHeader = $( "<header>" );
-    const $headerImg = $( "<img/>" ).attr( "src", tweetData.user.avatars.small );
+    const $headerImg = $( "<img/>" ).attr( "src", tweet.user.avatars.small );
     const $headerDiv = $( "<div>" ).addClass( "user-info" );
-    const $headDivH2 = $( "<h2>" ).text(tweetData.user.name);
-    const $headDivP = $( "<p>" ).text(tweetData.user.handle);
+    const $headDivH2 = $( "<h2>" ).text( tweet.user.name );
+    const $headDivP = $( "<p>" ).text( tweet.user.handle );
     const $postFieldDiv = $( "<div>" ).addClass( "post-field" );
-    const $pFDivP = $( "<p>" ).text(tweetData.content.text);
+    const $pFDivP = $( "<p>" ).text( tweet.content.text );
     const $eachTweetFooter = $( "<footer>" );
-    const $footerP = $( "<p>" ).addClass( "time-stamp" ).text(tweetData.created_at);
+    const $footerP = $( "<p>" ).addClass( "time-stamp" ).text( `${tweet.created_at} days ago` );
 
     //builds each-tweet element structure
     const $fullHeaderDiv = $( $headerDiv ).append( $headDivH2, $headDivP );
@@ -42,18 +84,7 @@ $(function() {
     return $allTogetherNow;
   }
 
-  function renderTweets(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-
-  }
-
-  const $tweet = createTweetElement(tweetData);
-  // console.log($tweet);
-
-  $("#published-tweets").append($tweet);
-
+  renderTweets(tweetData);
 });
 
 //NOTES:
