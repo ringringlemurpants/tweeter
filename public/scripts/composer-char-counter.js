702b6b74cfa2a382d45feb7b843dbcdb9dca361e
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-24 01:29:28 +0000

    adds finished published-tweet boxes with dummy info

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,19 @@
 $(function() {
-  //reduce ".counter" by number of characters in ".tweet-input" textarea
-  $( ".tweet-input" ).keyup(function() {
+  //reduce "#counter" by number of characters in ".tweet-input" textarea
+  $( "#tweet-input" ).keyup(function() {
     //updates number in counter
     const tweetContent = $(this).val();
     const reducedCount = 140 - tweetContent.length;
-    $(this).siblings( ".counter" ).html(reducedCount);
-    //changes colour of counter ".counter" on length of ".tweet-input"
+    $(this).siblings( "#counter" ).html(reducedCount);
+    //changes colour of counter "#counter" on length of ".tweet-input"
     if (tweetContent.length > 139) {
-      $( ".counter" ).toggleClass( "red", true);
+      $( "#counter" ).toggleClass( "red", true);
     } else if (tweetContent.length > 129 && tweetContent.length < 140) {
-      $( ".counter" ).toggleClass( "orange", true);
-      $( ".counter" ).toggleClass( "red", false);
+      $( "#counter" ).toggleClass( "orange", true);
+      $( "#counter" ).toggleClass( "red", false);
     } else {
-      $( ".counter" ).toggleClass( "orange", false);
-      $( ".counter" ).toggleClass( "red", false);
+      $( "#counter" ).toggleClass( "orange", false);
+      $( "#counter" ).toggleClass( "red", false);
     }
   });
 });
