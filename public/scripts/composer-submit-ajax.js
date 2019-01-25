f22e2f5ffb566fa650a9f1c980f01cbabe5c5fec
Ghabe Bossin <gee.bossin@gmail.com>
2019-01-25 05:16:36 +0000

    adds validation if no tweet or over 140 characters

--- public/scripts/composer-submit-ajax.js
+++ public/scripts/composer-submit-ajax.js
@@ -2,21 +2,24 @@
 
 $(function() {
   $( "form" ).submit(function(event){
-
     event.preventDefault();
-    const tweetOut = $(this).children( "#tweet-input" ).serialize();
+    const tweet = $(this).children( "#tweet-input" );
+    const tweetOut = tweet.val();
+    const tweetOutSerial = tweet.serialize();
     console.log(tweetOut);
-
-    $.ajax({
-      method: "POST",
-      url: "/tweets",
-      data: tweetOut
-    })
-      .done(function() {
-        alert( "Data Saved!" );
+    if (tweetOut.length > 140) {
+      alert( "Twort-abort: twits tweet too long!" );
+    } else if (tweetOut.length < 1) {
+      alert( "You must enter a tweet to twort you twit!" );
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: tweetOutSerial
       })
-      .fail(function() {
-        alert( "You must enter a tweet to twort you twit!" );
-      });
+        .fail(function() {
+          alert( "Fail." );
+        });
+    }
   });
 });
